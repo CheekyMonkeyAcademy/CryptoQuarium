ad5d507a71a166b3452c63eb09555af3044d3269
neliza <nicolasaelf64@gmail.com>
2017-12-21 17:55:42 -0700

    tank component

---
+++ src/components/Tank/Tank.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import "./Aquarium.css";
+
+class Tank extends Component {
+    Post extends Component {
+        render(){
+            return (
+                <div className="container">
+                    <div className="row">
+                        This is my Tanl
+                    </div>
+                </div>
+            )
+}
+
+
+export default Tank;
