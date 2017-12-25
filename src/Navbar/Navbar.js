555547bb2c3612a1b30cec8d6173e52a287a11c7
neliza <nicolasaelf64@gmail.com>
2017-12-25 11:44:27 -0700

    Adding in Navbar

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+//Possible pages we will need:
+    //Login 
+    //User personal aquarium - "MyAquarium"
+    //Buy/Sell your fish
+    //Crypto "Bank Account"
+    //Site home page
+        //User logs in and has access to their own personal aquarium site
+
+const Navbar = props =>
+    <ul className="nav nav-tabs">
+        <li
+            onClick = {() => { props.handlePageChange("Login"); } }
+            className = {props.currentPage === "Login" ? "active": ""}
+        >
+            <a>Home</a>
+        </li>
+
+        <li
+            onClick = {() => {props.handlePageChange("MyAquarium"); }}
+            className = {props.currentPage === "MyAquarium" ? "active": ""}
+        >
+            <a>Home</a>
+        </li>
+
+        <li
+            onClick = {() => {props.handlePageChange("Buy|Sell|Trade"); }}
+            className = {props.currentPage === "Buy|Sell|Trade" ? "active": ""}
+        >
+            <a>Home</a>
+        </li>
+
+        <li
+            onClick = {() => {props.handlePageChange("CryptoAccount"); }}
+            className = {props.currentPage === "CryptoAccount" ? "active": ""}
+        >
+            <a>Home</a>
+        </li>
+
+        <li
+            onClick = {() => {props.handlePageChange("Home"); }}
+            className = {props.currentPage === "Home" ? "active": ""}
+        >
+            <a>Home</a>
+        </li>
+    </ul>;
+
+    export default Navbar;
