7b0b41943cd7a5e9a14eab45576ae596c8b6bf6f
Robert Castiblanco <ccrs2006@gmail.com>
2017-12-28 15:34:32 -0700

    Implemented user aquarium features

--- server/model to be implemented/aquariumTemplate.js
+++
@@ -1,7 +0,0 @@
-// Aquarium_Templates.js
-
-// These never change? So no need for a template?
-// Starter would be like a fish bowl - small / no decoration
-// - Tank_Description (round bowl, small box, medium box, etc)
-// - Num_Fish_Allowed
-// - Num_Decoration_Allowed
---
+++ server/model to be implemented/userAquarium.js
@@ -0,0 +1,62 @@
+// User_Aquarium_Inventory.js
+
+// Inherits template and modifies it
+// - User_Owner (ID reference)
+// - Tank_Description (round bowl, small box, medium box, etc)
+// - Volume (describes amount of water for user reference)
+// - Num_Fish_Allowed
+// - Num_Decoration_Allowed
+// - Background
+// - Lighting_Effect (psycadellic baby! Nicole and Robert, make this happen)
+// --- Fish assigned / decorations assigned, etc
+
+
+module.exports = function(sequelize, DataTypes) {
+    let Aquarium = sequelize.define("aquarium", {
+        tankDescription: {
+            type: DataTypes,STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        numFishAllowed: {
+            type: DataTypes,INTEGER,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        numDecorationsAllowed: {
+            type: DataTypes,INTEGER,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        numDecorationsAllowed: {
+            type: DataTypes,INTEGER,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        numDecorationsAllowed: {
+            type: DataTypes,INTEGER,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        numDecorationsAllowed: {
+            type: DataTypes,INTEGER,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        }
+    });
+
+
+    return Aquarium;
+};
