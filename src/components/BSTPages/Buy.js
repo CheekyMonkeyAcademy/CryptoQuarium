4130af970c883530e8d632cc15c1e58c38f21469
neliza <nicolasaelf64@gmail.com>
2017-12-31 22:56:11 -0700

    Whoop! Whoop! Another Win! The cart subtotal updated when a user adds an item to a cart

--- src/components/BSTPages/Buy.js
+++ src/components/BSTPages/Buy.js
@@ -25,6 +25,9 @@ class Buy extends Component {
         this.setState({cartArray: this.state.cartArray.concat([this.state.buyFishArray[id-1]])}, () => {
             console.log("This is cart array")
             console.log(this.state.cartArray)  
+        //Call the update subtotal function here!
+            //Everytime the cart state is updated, the subtotal gets updated too!
+            this.updateSubtotalState()
         }); 
     }    
 
@@ -42,6 +45,21 @@ class Buy extends Component {
             console.log(err)
         })       
     }
+
+    //FUNCTION TO HANDLE THE SUBTOTAL MATH
+    updateSubtotalState = () => {
+        // Loop over the cart array to find the price of each item in there
+        this.state.cartArray.forEach((item) => {
+            console.log("this is item price")
+            console.log(item.price);
+
+            //Add the price (you will need to burrow into each fish object to grab the price)
+            const finalSubtotal = this.state.subTotal + item.price
+
+            //Update the state of the subtotal
+            this.setState({subTotal: finalSubtotal})         
+        });          
+    }    
 
     render(){        
         return (             
@@ -63,9 +81,9 @@ class Buy extends Component {
                     </div>
 
                     <div className="col s4">
-                        <BuyCart 
-                            shoppingCart={this.state.cartArray}   
-                            subTotal={this.state.subTotal}                          
+                        <BuyCart                               
+                            shoppingCart={this.state.cartArray}  
+                            subTotal={this.state.subTotal}                                                                             
                         />
                     </div>
                 </div>
