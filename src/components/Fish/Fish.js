9a024d33914d44a8f1b9b312c4e999be8d69efd7
Robert Rollner <robrollner@gmail.com>
2017-12-31 17:33:35 -0700

    tank and fish, need to add state and more fish

--- src/components/Fish/Fish.js
+++ src/components/Fish/Fish.js
@@ -1,13 +1,23 @@
-import React, {Component} from 'react';
-import './Fish.css';
+import React, {Component} from "react";
 
+import "./Fish.css";
 
 class Fish extends Component {
-    render(){
-        return(
-           <div className="fish">
-                Fish
-           </div>
+    render() {
+        return (
+            <div className = "fish_wrap" > 
+                <div className="fish1">
+                    <div className="top_fin"></div>
+                    <div className="tail_fin"></div>
+                    <div className="fish_body">
+                        <div className="eye"></div>
+                        <div className="scale_1"></div>
+                        <div className="scale_2"></div>
+                        <div className="scale_3"></div>
+                        <div className="scale_4"></div>
+                    </div>
+                </div> 
+            </div>
         )
     }
 };
--- src/components/Tank/Tank.js
+++ src/components/Tank/Tank.js
@@ -1,30 +1,64 @@
 import React, {Component} from "react";
-// import Water from "../Water";
 import Fish from "../Fish";
 import "./Tank.css";
 
 class Tank extends Component {
-        render(){
-            return (
-                <div className="container">
-                    <div className="row">
-                        <div className="tankBorder">
-                            <div id="box">
-                                <figure class="front"></figure>
-                                <figure class="back"></figure>
-                                <figure class="right"></figure>
-                                <Fish>Fish</Fish>
-                                {/* <Water> Water </Water> */}
-                                <figure class="left"></figure>
-                                <figure class="top"></figure>
-                                <figure class="bottom"></figure>                                
-                            </div>                            
-                        </div>
-                    </div>
-                </div>
-            )
-        }
-    };
+    render() {
+        return (
+<div id="wrap"> 
+<div className="window">
+    <div className="fishtank_wrap">
+        <div className="fishtank">
+        <div className="bubble_wrap">
+        <div className="bubbles b0"></div>
+        <div className="bubbles b1"></div>
+        <div className="bubbles b2"></div>
+        <div className="bubbles b3"></div>
+        <div className="bubbles b4"></div>
+        <div className="bubbles b5"></div>
+        </div>
+        <div className="bubble_wrap bw2">
+        <div className="bubbles b6"></div>
+        <div className="bubbles b7"></div>
+        <div className="bubbles b8"></div>
+        </div>
+        <div className="water"></div>
+        <Fish></Fish>
+        
 
+            <div className="ground"></div>
+            <div className="rock_1"></div>
+            <div className="rock_2"></div>
+            <div className="rock_3"></div>
+            <div className="rock_4"></div>
+            <div className="rock_5"></div>
+            <div className="plant_1_wrap">
+                <div className="plant_1"></div>
+                <div className="plant_2"></div>
+                <div className="plant_3"></div>
+            </div>
+            <div className="plant_2_wrap">
+                <div className="plant_4"></div>
+                <div className="plant_5"></div>
+            </div>
+            <div className="long_plant">
+                <div className="l_plant_1"></div>
+                <div className="l_plant_2"></div>
+                <div className="l_plant_3"></div>
+            </div>
+            <div className="long_plant2">
+
+                <div className="l_plant_2"></div>
+                <div className="l_plant_3"></div>
+            </div>
+            <div className="water"></div>
+
+        </div>
+    </div>
+</div>
+</div>
+        )
+    }
+};
 
 export default Tank;
