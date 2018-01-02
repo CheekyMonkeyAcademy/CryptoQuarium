1201670e5addfd78d52f1c4e6d403fa6e5b0b3d8
Kyle Kowalski <Kyle.Kowalski@gmail.com>
2018-01-02 12:14:58 -0700

    fix for the buy.js (index fix for purchase)

--- src/components/BSTPages/Buy.js
+++ src/components/BSTPages/Buy.js
@@ -12,31 +12,25 @@ class Buy extends Component {
         subTotal: 0
     }    
 
-
-    //MASSIVE TODO!!! THIS WORKS FOR NOW- BUT WILL DEFINITELY NEED TO BE REVISITED
-        //ID FOR FISH START AT 1,2,3 ETC
-        //AND THE ARRAY STARTS WITH 0,1,2 ETC
-        //CURRENTLY ADDED IN -1 TO FIX- BUT WILL CREATE PROBLEMS LATER DOWN THE ROAD
-        //POSSIBLE FIX: this.state.fishThing[this.state.filter(filter to get the specific index of the matching ID)]
-            //A FILTER OF THE ARRAY TO LOOK FOR THE APPROPRIATE ID ASSOCIATED WITH THE FISH
     clickItem = (id) => {
-        console.log("You got here")
-        console.log(`clicked: ${id}`)
-        this.setState({cartArray: this.state.cartArray.concat([this.state.buyFishArray[id-1]])}, () => {
-            console.log("This is cart array")
-            console.log(this.state.cartArray)  
+        // We are looking for the index of the target fihs... so... find index of all fish where the fish is filtered to the fish with the target ID
+        // This will prevent issues when we concatenate below.  
+        let fishIndex = this.state.buyFishArray.findIndex((fish) => fish===this.state.buyFishArray.filter(fish => fish.id===id)[0]);
+        this.setState({cartArray: this.state.cartArray.concat([this.state.buyFishArray[fishIndex]])}, () => {
+            // console.log("This is cart array")
+            // console.log(this.state.cartArray)  
         }); 
     }    
 
     updateBuyFishArrayState = () => {
         axios.get('/api/allFishTemplates')
         .then((allfish) => {
-            console.log(allfish);
+            // console.log(allfish);
                 allfish.data.forEach((fish) => {
                     this.setState({buyFishArray: this.state.buyFishArray.concat([fish])})
                 })
-                console.log("This is fish array")
-                console.log(this.state.buyFishArray)
+                // console.log("This is fish array")
+                // console.log(this.state.buyFishArray)
         })
         .catch((err)=> {
             console.log(err)
