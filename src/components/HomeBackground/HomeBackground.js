9c27f2fa46b07a2c6c3cdf0357679287b83130c4
Robert Castiblanco <ccrs2006@gmail.com>
2018-01-04 00:42:20 -0700

    Played a little with home page design, also tried to edit the logo on the nav, and very quick added the SQL drop tables file

---
+++ src/components/HomeBackground/HomeBackground.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./HomeBackground.css";
+
+
+const HomeBackground = props =>
+    <div className="home">
+        {props.children}
+    </div>
+
+export default HomeBackground;
