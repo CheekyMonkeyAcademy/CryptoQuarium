95d2882a63aa1424b44eca90aed88b7c384c159b
Kyle Kowalski <Kyle.Kowalski@gmail.com>
2018-01-07 10:08:54 -0700

    db update looks good, now to finish creation of user fish

--- SQL_PopulateDBWithDummyData.sql
+++ SQL_PopulateDBWithDummyData.sql
@@ -13,8 +13,20 @@ VALUES  ('gravel', '2.50', '2018-1-1', '2018-1-1'),
         ('submarine', '5.99', '2018-1-1', '2018-1-1'),
         ('rustyBike', '7.97', '2018-1-1', '2018-1-1'),
         ('anchor', '5.49', '2018-1-1', '2018-1-1');
+
 -- The following are optional for future use
 -- 'rock', 'castle', 'sunkenShip','bubbleMarker', 'teeterTotter', 'corucopia', 'roboticTurkey', 'diver', 'hookHand', 'pillars', 'aquiaticPlant', 'driftWood', 'startFish', 'anemore', 'harpoon', 'hotAirBallon', 'trafficCone', 'lantern', 'beachBall', 'windMill', 'pirateSkull', 'mermaid', 'pyramid', 'necklace', 'treasure', 'golfBall');
+
+
+
+
+-- This sets DEFAULT values for fish in the Aquarium - key pieces:  
+-- -- codeSpecies:  This MUST match the CSS/Fish Name - this is how we map the components ---- we can use this to sub in differnt fish models later easily
+-- -- randomizeVar:  This is a variable for  what values WILL BE RANDOMIZED on fish creation.  
+-- -- -- format for this:  {color:[array of colors IE color1r, color2b, etc], percent:true/false: degree: true/false}
+INSERT INTO `cryptoquarium_db`.`fishes` (`species`, `codeSpecies`, `color1r`, `color1b`, `color1g`, `color2r`, `color2b`, `color2g`, `degree`, `percent`, `randomizeVar`, `quantityAvailable`, `price`, `createdAt`, `updatedAt`) VALUES ('Fish', 'Fish', '255', '94', '0', '2', '83', '235', '145', '72', '{color: [color2r, color2b, color2g], degree: true, percent: true}', '1000', '1', '2018-1-7', '2018-1-7');
+INSERT INTO `cryptoquarium_db`.`fishes` (`species`, `codeSpecies`, `color1r`, `color1b`, `color1g`, `color2r`, `color2b`, `color2g`, `degree`, `percent`, `randomizeVar`, `quantityAvailable`, `price`, `createdAt`, `updatedAt`) VALUES ('Puffer Fish', 'PufferFish', '0', '0', '0', '242', '237', '136', '180', '50', '{color: [color2r, color2b, color2g], degree: false, percent: false}', '1000', '1', '2018-1-7', '2018-1-7');
+
 
 -- INSERT INTO userAquiarium ('name', 'tankDescription', 'numFishAllowed', 'numDecorationsAllowed', 'background', 'lightingEffect');
 -- VALUES  ('', '', '', '', '', ''),
@@ -23,9 +35,3 @@ VALUES  ('gravel', '2.50', '2018-1-1', '2018-1-1'),
 --         ('', '', '', '', '', ''),
 --         ('', '', '', '', '', '');
 
--- INSERT INTO fish ('species', 'image', 'movementMin', 'movementMax', 'movementPercent', 'movementHeightMin'. 'movementHeightMax', 'quantityAvailable', 'price');
--- VALUES  ('', '', '', '', '', '', '', '', ''),
---         ('', '', '', '', '', '', '', '', ''),
---         ('', '', '', '', '', '', '', '', ''),
---         ('', '', '', '', '', '', '', '', ''),
---         ('', '', '', '', '', '', '', '', '');
