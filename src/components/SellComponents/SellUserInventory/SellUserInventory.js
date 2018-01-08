1db44f638e3e726f5e0c6a6248de256081f57ca5
neliza <nicolasaelf64@gmail.com>
2018-01-08 09:42:55 -0700

    beginning sell page

--- src/components/SellComponents/SellUserInventory/SellUserInventory.js
+++ src/components/SellComponents/SellUserInventory/SellUserInventory.js
@@ -7,6 +7,11 @@ class SellUserInventory extends Component {
         myFishArray: []
     }
 
+//  You can run:  app.post('/api/userPurchaseFish/:id'
+// ID being that of target fish
+// (usually 1 or 2)
+
+    
     componentDidMount(){
          axios.get('/api/allUserFish') 
          .then((allUserFish) => {
