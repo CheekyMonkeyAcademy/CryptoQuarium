a7aed5bfa9faad95513c559da62f2eb0cfba3cd1
Kyle Kowalski <Kyle.Kowalski@gmail.com>
2018-01-10 17:09:38 -0700

    fixed async issue for buying from market - untested code for buying from other users - back in a few

--- server/routes/fishApiRoutes.js
+++ server/routes/fishApiRoutes.js
@@ -38,62 +38,67 @@ module.exports = function(app) {
 
     // TODO TEST THIS ENTIRE BEAST
     app.post('/api/userPurchaseOtherUserFish/', function(req, res){
-        // First we go through the array of fish and we start breaking them down to individual buys
         
-        req.body.forEach((fish) => {
-            // TODO:  Fix this - it's an async mess.  
-            // Basically... you call it and it executes them all at the same time - BAD
-            // let fish = {} // --- TEST VALUE
-            // fish.id = 65; // --- TEST VALUE
-            db.UserFish.findOne({
-                where: {
-                    id: fish.id
-                }
-            })
-            .then((fishForSale) => {
-                // Get the user wanting to purchase
-                db.User.findOne({
-                    where: {
-                        id: req.user.id
-                        // id: 1 // --- TEST VALUE
-                    }
-                })
-                .then((userBuying) => {
-                    if (userBuying.walletBalance >= fishForSale.price) {
-                        // Money changes hands
-                        userBuying.update({walletBalance: (userBuying.walletBalance -= fishForSale.price)});
-                        db.WalletHistory.create({
-                            UserId: req.user.id,
-                            // UserId: 1, //--- TEST VALUE
-                            walletBalanceChange: -fishForSale.price,
-                            walletBalanceChangeReason: `Fish purchased from other user: ${fishForSale.species} named: '${fishForSale.name}'`,
-                            lastWalletBalance: (userBuying.walletBalance - fishForSale.price)
-                        });
+        asyncOtherUserFishPurchase(req.body, req.user.id)
+        .then((asyncReturn) => {
+            console.log(`@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@`);
+            res.sendStatus(204);
+        })
+        .catch((error) => {
+            console.log(`BOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO`);
+            res.sendStatus(400).json(error);
+        })
 
-                        db.User.findOne({
-                            where: {
-                                id: fishForSale.UserId
-                            }
-                        })
-                        .then((userSelling) => {
-                            userSelling.update({walletBalance: (userSelling.walletBalance += fishForSale.price)});
-                            db.WalletHistory.create({
-                                UserId: userSelling.id,
-                                walletBalanceChange: +fishForSale.price,
-                                walletBalanceChangeReason: `Fish sold: ${fishForSale.species} named: '${fishForSale.name}'`,
-                                lastWalletBalance: (userSelling.walletBalance + fishForSale.price)
-                            });
-                        });
+        // req.body.forEach((fish) => {
+        //     db.UserFish.findOne({
+        //         where: {
+        //             id: fish.id
+        //         }
+        //     })
+        //     .then((fishForSale) => {
+        //         // Get the user wanting to purchase
+        //         db.User.findOne({
+        //             where: {
+        //                 id: req.user.id
+        //                 // id: 1 // --- TEST VALUE
+        //             }
+        //         })
+        //         .then((userBuying) => {
+        //             if (userBuying.walletBalance >= fishForSale.price) {
+        //                 // Money changes hands
+        //                 userBuying.update({walletBalance: (userBuying.walletBalance -= fishForSale.price)});
+        //                 db.WalletHistory.create({
+        //                     UserId: req.user.id,
+        //                     // UserId: 1, //--- TEST VALUE
+        //                     walletBalanceChange: -fishForSale.price,
+        //                     walletBalanceChangeReason: `Fish purchased from other user: ${fishForSale.species} named: '${fishForSale.name}'`,
+        //                     lastWalletBalance: (userBuying.walletBalance - fishForSale.price)
+        //                 });
 
-                        // Fish is moved from original user to current user
-                        fishForSale.update({UserId: req.user.id});
-                        // fishForSale.update({UserId: 1}); // --- TEST VALUE
-                        fishForSale.update({forSale: false});
-                        res.json({"Success": "Fish has changed hands"});
-                    }
-                });
-            });
-        });
+        //                 db.User.findOne({
+        //                     where: {
+        //                         id: fishForSale.UserId
+        //                     }
+        //                 })
+        //                 .then((userSelling) => {
+        //                     userSelling.update({walletBalance: (userSelling.walletBalance += fishForSale.price)});
+        //                     db.WalletHistory.create({
+        //                         UserId: userSelling.id,
+        //                         walletBalanceChange: +fishForSale.price,
+        //                         walletBalanceChangeReason: `Fish sold: ${fishForSale.species} named: '${fishForSale.name}'`,
+        //                         lastWalletBalance: (userSelling.walletBalance + fishForSale.price)
+        //                     });
+        //                 });
+
+        //                 // Fish is moved from original user to current user
+        //                 fishForSale.update({UserId: req.user.id});
+        //                 // fishForSale.update({UserId: 1}); // --- TEST VALUE
+        //                 fishForSale.update({forSale: false});
+        //                 res.json({"Success": "Fish has changed hands"});
+        //             }
+        //         });
+        //     });
+        // });
     });
                 
 
@@ -104,7 +109,6 @@ module.exports = function(app) {
         // 2. See if there are quantity quantityAvailable
         // 3. If it is, see if the user has the required funds
         // 4. If all of that works, buy it and move along 
-
         
         asyncFishPurchase(req.body, req.user.id)
         .then((asyncReturn) => {
@@ -174,52 +178,58 @@ asyncFishPurchase = (fishArray, userId, index = 0, deferred = Q.defer()) => {
             .then((user) => {
                 if (user.walletBalance >= selectedFish.price) {
                     console.log(`You're clear to buy this fish, w00t!`);
-                    selectedFish.update({quantityAvailable: (selectedFish.quantityAvailable -= 1)});
-                    user.update({walletBalance: (user.walletBalance -= selectedFish.price)});
-                    db.WalletHistory.create({
-                        UserId: userId,
-                        walletBalanceChange: -selectedFish.price,
-                        walletBalanceChangeReason: `Fish purchased: ${selectedFish.species}`,
-                        lastWalletBalance: user.walletBalance
-                    });
-                    let randomizeThese = JSON.parse(selectedFish.randomizeVar);
-                    let randomizedTargets = {};
-                    if (randomizeThese.percent === true) {
-                        console.log(`We are randomizing PERCENT on this fish`);
-                        randomizedTargets.percent = randomize(0,100);
-                    }
-                    if (randomizeThese.degree === true) {
-                        console.log(`We are randomizing DEGREE on this fish`);
-                        randomizedTargets.degree = randomize(1,360);
-                    }
-                    randomizeThese.color.forEach((color) => {
-                        console.log(`We are randomizing THIS COLOR: ${color}`);
-                        randomizedTargets[color] = randomize(1, 255);
+                    selectedFish.update({quantityAvailable: (selectedFish.quantityAvailable -= 1)})
+                    .then(() => {
+                        user.update({walletBalance: (user.walletBalance -= selectedFish.price)})
+                        .then(() => {
+                            db.WalletHistory.create({
+                                UserId: userId,
+                                walletBalanceChange: -selectedFish.price,
+                                walletBalanceChangeReason: `Fish purchased: ${selectedFish.species}`,
+                                lastWalletBalance: user.walletBalance
+                            })
+                            .then(() => {
+                                let randomizeThese = JSON.parse(selectedFish.randomizeVar);
+                                let randomizedTargets = {};
+                                if (randomizeThese.percent === true) {
+                                    console.log(`We are randomizing PERCENT on this fish`);
+                                    randomizedTargets.percent = randomize(0,100);
+                                }
+                                if (randomizeThese.degree === true) {
+                                    console.log(`We are randomizing DEGREE on this fish`);
+                                    randomizedTargets.degree = randomize(1,360);
+                                }
+                                randomizeThese.color.forEach((color) => {
+                                    console.log(`We are randomizing THIS COLOR: ${color}`);
+                                    randomizedTargets[color] = randomize(1, 255);
+                                })
+                                console.log(randomizedTargets);
+            
+                                db.UserFish.create({
+                                    name: 'New Fish Needs A Name',
+                                    species: selectedFish.species,
+                                    codeSpecies: selectedFish.codeSpecies,
+                                    color1r: randomizedTargets.color1r ? randomizedTargets.color1r : selectedFish.color1r,
+                                    color1b: randomizedTargets.color1b ? randomizedTargets.color1b : selectedFish.color1b,
+                                    color1g: randomizedTargets.color1g ? randomizedTargets.color1g : selectedFish.color1g,
+                                    color2r: randomizedTargets.color2r ? randomizedTargets.color2r : selectedFish.color2r,
+                                    color2b: randomizedTargets.color2b ? randomizedTargets.color2b : selectedFish.color2b,
+                                    color2g: randomizedTargets.color2g ? randomizedTargets.color2g : selectedFish.color2g,
+                                    degree: randomizedTargets.degree ? randomizedTargets.degree : selectedFish.degree,
+                                    percent: randomizedTargets.percent ? randomizedTargets.percent : selectedFish.percent,
+                                    forSale: false,
+                                    price: 0,
+                                    UserId: userId
+                                })
+                                .then((fishObject) => {
+                                    console.log(`Fish purchased`);
+                                    // move the index along and fire again
+                                    index++
+                                    asyncFishPurchase(fishArray, userId, index, deferred);
+                                });
+                            })
+                        })
                     })
-                    console.log(randomizedTargets);
-
-                    db.UserFish.create({
-                        name: 'New Fish Needs A Name',
-                        species: selectedFish.species,
-                        codeSpecies: selectedFish.codeSpecies,
-                        color1r: randomizedTargets.color1r ? randomizedTargets.color1r : selectedFish.color1r,
-                        color1b: randomizedTargets.color1b ? randomizedTargets.color1b : selectedFish.color1b,
-                        color1g: randomizedTargets.color1g ? randomizedTargets.color1g : selectedFish.color1g,
-                        color2r: randomizedTargets.color2r ? randomizedTargets.color2r : selectedFish.color2r,
-                        color2b: randomizedTargets.color2b ? randomizedTargets.color2b : selectedFish.color2b,
-                        color2g: randomizedTargets.color2g ? randomizedTargets.color2g : selectedFish.color2g,
-                        degree: randomizedTargets.degree ? randomizedTargets.degree : selectedFish.degree,
-                        percent: randomizedTargets.percent ? randomizedTargets.percent : selectedFish.percent,
-                        forSale: false,
-                        price: 0,
-                        UserId: userId
-                    })
-                    .then((fishObject) => {
-                        console.log(`Fish purchased`);
-                        // move the index along and fire again
-                        index++
-                        asyncFishPurchase(fishArray, userId, index, deferred);
-                    });
                 }
                 else {
                     let returnError = {"Error": "Insufficient funds to purchase this fish"}
@@ -231,6 +241,75 @@ asyncFishPurchase = (fishArray, userId, index = 0, deferred = Q.defer()) => {
     return deferred.promise;
 }
 
-asyncUserFishPurchase = () => {
-    return true;
+asyncOtherUserFishPurchase = (fishArray, userId, index = 0, deferred = Q.defer()) => {
+    console.log(`total length: ${fishArray.length}`);
+    console.log(`Calling fish purchase async like.. round ${index}.`);
+
+    if (index >= fishArray.length) {
+        console.log(`ANNNDD we're done here!`);
+        deferred.resolve();
+        return "Success";
+    }
+    else {
+        console.log(`Purchase fish here ${fishArray[index]}`);
+
+        db.UserFish.findOne({
+            where: {
+                id: fishArray[index]
+            }
+        })
+        .then((fishForSale) => {
+            // Get the user wanting to purchase
+            db.User.findOne({
+                where: {
+                    id: userId
+                }
+            })
+            .then((userBuying) => {
+                if (userBuying.walletBalance >= fishForSale.price) {
+                    // Money changes hands
+                    userBuying.update({walletBalance: (userBuying.walletBalance -= fishForSale.price)})
+                    .then(() => {
+                        db.WalletHistory.create({
+                            UserId: userId,
+                            walletBalanceChange: -fishForSale.price,
+                            walletBalanceChangeReason: `Fish purchased from other user: ${fishForSale.species} named: '${fishForSale.name}'`,
+                            lastWalletBalance: (userBuying.walletBalance)
+                        })
+                        .then(() => {
+                            db.User.findOne({
+                                where: {
+                                    id: fishForSale.UserId
+                                }
+                            })
+                            .then((userSelling) => {
+                                userSelling.update({walletBalance: (userSelling.walletBalance += fishForSale.price)})
+                                .then(() => {
+                                    db.WalletHistory.create({
+                                        UserId: userSelling.id,
+                                        walletBalanceChange: +fishForSale.price,
+                                        walletBalanceChangeReason: `Fish sold: ${fishForSale.species} named: '${fishForSale.name}'`,
+                                        lastWalletBalance: (userSelling.walletBalance)
+                                    })
+                                    .then(() => {
+                                        // Fish is moved from original user to current user 
+                                        fishForSale.update({UserId: userId});
+                                        fishForSale.update({forSale: false});
+                                        // move the index along and fire again
+                                        index++
+                                        asyncOtherUserFishPurchase(fishArray, userId, index, deferred);
+                                    });
+                                });
+                            });
+                        })
+                    })
+                }
+                else {
+                    let returnError = {"Error": "Insufficient funds to purchase this fish"}
+                    deferred.reject(returnError);
+                }
+            });
+        });
+    }
+    return deferred.promise;
 }
