fb3c83cdd89f5640c29c9ae72fc20fc23209e006
Kyle Kowalski <Kyle.Kowalski@gmail.com>
2018-01-12 16:22:58 -0700

    Update LoginBackground.js

--- src/components/LoginBackground/LoginBackground.js
+++ src/components/LoginBackground/LoginBackground.js
@@ -5,7 +5,7 @@ import { button } from 'react-materialize';
 const LoginBackground = props =>
     <div className="login">
             <div className="button">
-                <a method='POST' href='http://localhost:8080/auth/google'>
+                <a method='POST' href='/auth/google'>
                     <button className="btn btn-large red waves-effect"><i className="material-icons right">add</i>SIGN-IN WITH GOOGLE</button>
                 </a>        
             </div>
