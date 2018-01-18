646fff170b8d8604bf6c36684d6641ee4caf0d09
neliza <nicolasaelf64@gmail.com>
2018-01-18 14:36:50 -0700

    remove from market

--- src/components/SellComponents/SellCards/SellCards.js
+++ src/components/SellComponents/SellCards/SellCards.js
@@ -43,67 +43,8 @@ class SellCards extends Component {
             </div>
         </div>
     </div>
-
-
         )
     }
 }
 
 export default SellCards;
-{/* <div className="input-field col s4">
-<input onChange={() => this.props.handlePriceChange()} placeholder={this.props.price} type="number" min="0" max="9999999" className="validate" />
-</div>
-<a className="btn-floating halfway-fab waves-effect waves-light red" onClick={() => this.props.thisItemToMarket(this.props.id)}>
-<i className="material-icons">local_offer</i>
-</a> */}
-
-
-                                {/* <div className="row">
-                                    <input type="number" value={props.newPrice} id={"key" + props.id}  onChange={props.handlePriceChange}
-                                     />
-                                </div> */}
-
-// -------------------------THIS WAS USED PRIOR TO ADDING CSS---------------------------
-
-//     <div>
-//         <div className="row">
-//             <div className="col s12 m6">
-//                 <div className="card cyan lighten-5">
-//                     <div className="card-action">
-//                         <div className="row">
-//                             <div className="col s12">
-//                                 <div className="row">
-//                                     <div className="col s12">
-//                                         {props.species}
-//                                     </div>
-//                                 </div>
-//                                 <div className="row">
-//                                     <div className="col s12">
-//                                         {props.id}
-//                                     </div>
-//                                 </div>
-//                                 <form className="row">
-//                                     <div className="input-field col s4">
-//                                         <input onChange={() => props.handlePriceChange()} placeholder={props.price} type="number" min="0" max="9999999" className="validate" />
-//                                     </div>
-//                                     <a className="btn-floating halfway-fab waves-effect waves-light red" onClick={() => props.thisItemToMarket(props.id)}>
-//                                         <i className="material-icons">local_offer</i>
-//                                     </a>
-//                                 </form>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     </div>
-// );
-
-// export default SellCards;
-
-// <a className="btn-floating halfway-fab waves-effect waves-light red" onClick={() => props.thisItemToMarket(props.id)}>
-// <i className="material-icons">local_offer</i>
-// </a>
-
-// -------------------------^^^THIS WAS USED PRIOR TO ADDING CSS^^^---------------------------
-
