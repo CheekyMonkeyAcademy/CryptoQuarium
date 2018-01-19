b2a83d88b868875ea98e280f58982805466cfc91
Kyle Kowalski <Kyle.Kowalski@gmail.com>
2018-01-19 09:37:53 -0700

    fish are in boxes and in the tank.  Woot!

--- src/components/AllFish/PufferFish/PufferFish.js
+++ src/components/AllFish/PufferFish/PufferFish.js
@@ -68,17 +68,19 @@ class PufferFish extends Component {
 
     render() {
         return (
-            <div className = "fish_wrap2" id={this.props.id}> 
-                <div className="fish2">
-                    <div className="tail_fin2"></div>
-                    <div className="fish_body2">
-                        <div className="eye2"></div>
-                        <div className="scale_1_2"></div>
-                        <div className="scale_2_2"></div>
-                        <div className="scale_3_2"></div>
-                        <div className="scale_4_2"></div>
-                    </div>
-                </div> 
+            <div className="lowerFishToTank2">
+                <div className = "fish_wrap2" id={this.props.id}> 
+                    <div className="fish2">
+                        <div className="tail_fin2"></div>
+                        <div className="fish_body2">
+                            <div className="eye2"></div>
+                            <div className="scale_1_2"></div>
+                            <div className="scale_2_2"></div>
+                            <div className="scale_3_2"></div>
+                            <div className="scale_4_2"></div>
+                        </div>
+                    </div> 
+                </div>
             </div>
         )
     }
