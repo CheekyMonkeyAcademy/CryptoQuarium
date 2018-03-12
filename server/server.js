2d873544a6b25a763ca81800165ce5b8649dbd69
Kyle Kowalski <Kyle.Kowalski@gmail.com>
2018-03-12 12:30:14 -0600

    furhter updates

--- server/server.js
+++ server/server.js
@@ -42,6 +42,7 @@ const options = {
 let dbConnectionInfo = {}
 
 if (process.env.NODE_ENV == 'production') {
+    console.log(`$$$$$$$$$$$$$$$$$$$ FOUND: ${process.env.PROD_DB_HOST} $$$$$$$$$$$$$$$$$$$`)
     dbConnectionInfo = {
         host: process.env.PROD_DB_HOST,
         port: 3306,
